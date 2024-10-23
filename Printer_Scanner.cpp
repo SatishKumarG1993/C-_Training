d1596b61c3dba7d5020f4e5cb07c0acc67194463
SatishKumarG1993 <SatishKumar.guntreddi@in.bosch.com>
2024-10-23 14:22:14 +0530

    Update Printer_Scanner.cpp

--- Printer_Scanner.cpp
+++ Printer_Scanner.cpp
@@ -15,11 +15,13 @@ class Printer{
     Printer(string name,string location,string model,string type):_name(name),_location(location), _model(model),
     _type(type){
         cout<<"Printer Overloading constructor"<<endl;
+        }
+    void Print(){
         cout<<"NAME:"<<_name<<endl;
         cout<<"Location:"<<_location<<endl;
         cout<<"Model:"<<_model<<endl;
         cout<<"Type:"<<_type<<endl;
-        }
+    }
 };
 class Scanner{
     private:
@@ -32,16 +34,21 @@ class Scanner{
     }
     Scanner(string name,string location,string model):_name(name),_location(location), _model(model){
         cout<<"Scanner Overloading constructor"<<endl;
+        }
+        
+    void Scan(){
         cout<<"NAME:"<<_name<<endl;
         cout<<"Location:"<<_location<<endl;
         cout<<"Model:"<<_model<<endl;
-        }
+    }
 };
 int main(){
 Printer p1;
 Printer p2("BoschP1","First Floor","XYZ","ABC");
+p2.Print();
 Scanner s1;
 Scanner s2("BoschS1","First Floor","UVW");
+s2.Scan();
 
   return 0; 
 }
